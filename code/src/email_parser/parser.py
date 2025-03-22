1c70a2609b962f49319e298972060800c10a9a02
arnavnotfound <arnavchouhanofficial@gmail.com>
2025-03-22 16:59:16 +0530

    feat: email body now cleaned.

--- code/src/email_parser/parser.py
+++ code/src/email_parser/parser.py
@@ -1,6 +1,15 @@
 from models import Email, ParsedEmail
+import re
+
+
 
 def parse_email(email: Email) -> ParsedEmail:
+    body = email.body
+    cleaned_body = re.sub(r'\s+', ' ', body).strip() #removing \n new line symbols and extra spaces
+    body = cleaned_body
+
+    attachments = email.attachments
+
     """Extracts text & key fields from email body & attachments."""
     return ParsedEmail(
         email_id=email.email_id,
@@ -12,5 +21,13 @@ def parse_email(email: Email) -> ParsedEmail:
     )
 
 if __name__ == "__main__":
-    sample_email = Email(email_id="123", subject="Test", body="Transfer $5000", attachments=[], timestamp="2023-10-01T12:00:00Z", sender="j")
-    print(parse_email(sample_email).model_dump_json()) # For testing
+    sample_email = Email(email_id="123", 
+                         subject="Test", 
+                         body="       Transfer $5000 \n who is the best", 
+                         attachments=[], 
+                         timestamp="2023-10-01T12:00:00Z", 
+                         sender="j")
+
+    parse_email(sample_email)
+    
+    # print(parse_email(sample_email).model_dump_json()) # For testing
